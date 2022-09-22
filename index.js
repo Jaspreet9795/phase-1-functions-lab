14c4def82f58ae3c69fe3a7206f8969a7881e58f
Jaspreet Kaur <kaurjaspreet9795@gmail.com>
2022-09-22 16:05:25 -0700

    commit tree e2d0b109bc0f2d5f1d9303b215f95c393faef820

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const scuberHq = 42
+function distanceFromHqInBlocks(pickUp){
+    return  Math.abs(scuberHq-pickUp);
+}
+
+
+function distanceFromHqInFeet(pickUp){
+    let distanceFeet = distanceFromHqInBlocks(pickUp)
+    return distanceFeet*264
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination )*264)
+}
+
+
+function calculatesFarePrice(start, destination){
+    let feetTravel=distanceTravelledInFeet(start, destination)
+    if (feetTravel<=400){return 0}
+    else if (feetTravel<=2000){ return (feetTravel-400)*0.02}
+    else if (feetTravel<=2500){return 25}
+    else {return 'cannot travel that far'}
+    }
